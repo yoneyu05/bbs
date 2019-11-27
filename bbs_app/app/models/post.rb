bc85db1672c37f428859ce1f16794052c6a507f3
vagrant <vagrant@localhost.localdomain>
2019-11-27 18:50:11 +0900

    design v3

--- bbs_app/app/models/post.rb
+++ bbs_app/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :topic
-	default_scope -> { order(created_at: :desc) }
+	default_scope -> { order(created_at: :asc) }
 	 mount_uploader :image, ImageUploader
 	 validates :content, presence: true, length: {maximum: 10000}
 	 validates :topic_id, presence: true
