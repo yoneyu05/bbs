fdbac2f0976421d6bee5ff733f630ec08a973cde
Yu Yoneda <yoneyu05@gmail.com>
2020-01-13 22:38:46 +0000

    heroku v7

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
-    config.fog_provider = 'fog/aws'
+    #config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
@@ -13,6 +13,7 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
+    config.fog_use_ssl_for_aws = true
     config.fog_public     = false
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     config.fog_directory     =  ENV['S3_BUCKET']
