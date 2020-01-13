775017a783aacb2474d938672458169f4e792f0e
Yu Yoneda <yoneyu05@gmail.com>
2020-01-13 23:03:42 +0000

    heroku v8

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,7 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
-    #config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
